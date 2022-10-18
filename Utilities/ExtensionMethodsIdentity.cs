3fb5506d595bdf29a54653ae257c472eb060270e
Valerio Caporali <cyndoriil@gmail.com>
2022-10-18 17:46:06 +0200

    Utilities Implemented

---
+++ Utilities/ExtensionMethodsIdentity.cs
@@ -0,0 +1,47 @@
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.Utilities
+{
+    public static class ExtensionMethodIdentityId
+    {
+        public static int MaxBookIdValue(this IEnumerable<Book> enumerable)
+        {
+            int countId = 0;
+            foreach (var item in enumerable)
+            {
+                if (item.Id >= countId)
+                {
+                    countId = item.Id + 1;
+                }
+            }
+            return countId;
+        }
+
+        public static int MaxCustomerIdValue(this IEnumerable<Customer> enumerable)
+        {
+            int countId = 0;
+            foreach (var item in enumerable)
+            {
+                if (item.Id >= countId)
+                {
+                    countId = item.Id + 1;
+                }
+            }
+            return countId;
+        }
+
+        public static int MaxBorrowIdValue(this IEnumerable<Borrow> enumerable)
+        {
+            int countId = 0;
+            foreach (var item in enumerable)
+            {
+                if (item.Id >= countId)
+                {
+                    countId = item.Id + 1;
+                }
+            }
+            return countId;
+        }
+    }
+}
+
---
+++ Utilities/WriterReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace LibraryApi.Models
+{
+    public static class WriterReader
+    {
+        public static void Write(string inputText, string filePath)
+        {
+            using (var streamWriter = File.AppendText(filePath))
+            {
+                streamWriter.WriteLine(inputText);
+            }
+        }
+
+
+        public static List<T> Read<T>(string filePath)
+        {
+            List<T> list = new List<T>();
+            using var streamReader = new StreamReader(filePath);
+            while (!streamReader.EndOfStream)
+            {
+                var singleObject = JsonSerializer.Deserialize<T>(streamReader.ReadLine());
+                list.Add(singleObject);
+            }
+
+            return list;
+        }
+
+
+    }
+}
