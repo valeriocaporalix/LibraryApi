df92bf9ebcbde0d162cf8bda12ec29b544163422
Valerio Caporali <cyndoriil@gmail.com>
2022-10-19 09:10:30 +0200

    Borrow CRUD Implemented

--- Controllers/BorrowController.cs
+++ Controllers/BorrowController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryApi.Services;
+﻿using LibraryApi.Models;
+using LibraryApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryApi.Controllers
@@ -9,5 +10,44 @@ namespace LibraryApi.Controllers
     {
         
         private BorrowService _borrowService = new BorrowService();
+
+        [HttpGet()]
+        public IActionResult GetAll()
+        {
+            IEnumerable<Borrow> borrowList = _borrowService.GetAllBorrow();
+            return Ok(borrowList);
+        }
+
+        [HttpGet("{borrowId}")]
+        public IActionResult GetDetails(int borrowId)
+        {
+            Borrow borrow = _borrowService.GetBorrowById(borrowId);
+            if (borrow == null)
+                return NotFound();
+            return Ok(borrow);
+        }
+
+        [HttpPost()]
+        public IActionResult Post([FromBody] Borrow newBorrow)
+        {
+            _borrowService.AddBorrow(newBorrow);
+            return Created($"/{newBorrow.Id}", newBorrow);
+        }
+
+        [HttpDelete("{borrowId}")]
+        public IActionResult Delete(int borrowId)
+        {
+            _borrowService.DeleteBorrow(borrowId);
+            return NoContent();
+        }
+
+        [HttpPut("{borrowId}")]
+        public IActionResult Put([FromBody] Borrow borrow, int borrowId)
+        {
+            var borrowToUpdate = _borrowService.UpdateBorrow(borrowId, borrow);
+            if (borrowToUpdate != null)
+                return Created($"/{borrowId}", borrow);
+            return NoContent();
+        }
     }
 }
--- Services/BorrowService.cs
+++ Services/BorrowService.cs
@@ -1,6 +1,81 @@
-﻿namespace LibraryApi.Services
+﻿using LibraryApi.Models;
+using LibraryApi.Utilities;
+using System.Text.Json;
+
+namespace LibraryApi.Services
 {
     public class BorrowService
     {
+        private string _filePath = "./Files/Borrow.txt";
+
+        public void AddBorrow(Borrow newBorrow)
+        {
+            List<Borrow> list = WriterReader.Read<Borrow>(_filePath);
+            int countId = ExtensionMethodIdentityId.MaxBorrowIdValue(list);
+            newBorrow.Id = countId;
+            WriterReader.Write(JsonSerializer.Serialize(newBorrow), _filePath);
+        }
+
+        public IEnumerable<Borrow> GetAllBorrow()
+        {
+            var borrowList = WriterReader.Read<Borrow>(_filePath);
+            return borrowList;
+        }
+
+        public Borrow GetBorrowById(int borrowId)
+        {
+            List<Borrow> list = WriterReader.Read<Borrow>(_filePath);
+            var borrow = list.FirstOrDefault(borrow => borrow.Id == borrowId);
+            return borrow;
+        }
+
+        public void DeleteBorrow(int borrowId)
+        {
+            List<Borrow> borrowList = WriterReader.Read<Borrow>(_filePath);
+            File.WriteAllText(_filePath, string.Empty);
+
+            foreach (Borrow item in borrowList)
+            {
+                if (item.Id != borrowId)
+                {
+                    WriterReader.Write(JsonSerializer.Serialize(item), _filePath);
+                }
+            }
+        }
+
+        public Borrow UpdateBorrow(int borrowId, Borrow borrow)
+        {
+            List<Borrow> list = WriterReader.Read<Borrow>(_filePath);
+
+            int countId = list.MaxBorrowIdValue();
+
+            var borrowToUpdate = list.FirstOrDefault(borrow => borrow.Id == borrowId);
+
+
+            if (borrowToUpdate != null)
+            {
+                File.WriteAllText(_filePath, string.Empty);
+                foreach (Borrow item in list)
+                {
+                    if (item.Id != borrowToUpdate.Id)
+                    {
+                        WriterReader.Write(JsonSerializer.Serialize(item), _filePath);
+                    }
+                    else
+                    {
+                        borrow.Id = borrowToUpdate.Id;
+                        WriterReader.Write(JsonSerializer.Serialize(borrow), _filePath);
+                    }
+                }
+
+                return null;
+            }
+            else
+            {
+                borrow.Id = countId;
+                WriterReader.Write(JsonSerializer.Serialize(borrow), _filePath);
+                return borrow;
+            }
+        }
     }
 }
